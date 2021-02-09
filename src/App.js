6a7347421cfd32616f35a8fcaa7328a04256635c
spaghettiwews <spaghettiwews@outlook.com>
2021-02-09 22:35:44 +0200

    implement historical performance charts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { AreaChart, Area } from 'recharts'
 
 const Wrapper = styled.div`
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@500;600;800&display=swap');
@@ -29,6 +30,7 @@ width: 100%;
 & > input[type="text"] {
   background-color: #1c1c1e;
   border: none;
+  color: #8e8e92;
   width: 100%;
   border-radius: 8px;
   height: 34px;
@@ -50,7 +52,7 @@ padding: 0;
 & > li {
   border-bottom: 1px solid #323235;
   display: grid;
-  grid-template-columns: [name] 3fr [graph] 1fr [price] 1fr;
+  grid-template-columns: [name] 3fr [graph] 1.3fr [price] 1fr;
   padding: 18px 0;
 
   & > div {
@@ -113,23 +115,60 @@ const App = () => {
 
   const [counters, setCounters] = useState({})
   const [today] = useState(new Intl.DateTimeFormat('en-GB', { /*year: 'numeric',*/ month: 'long', day: 'numeric' }).format(Date.now()))
+  const [history, setHistory] = useState({})
+  // const [, forceUpdate] = useReducer(x => x + 1, 0)
 
   useEffect(() => {
-    fetch("http://localhost:8000/")
-    .then((response) => {
-      return response.json()
-    })
-    .then((json) => {
-      setCounters(json)
-    })
-    .catch((err) => {
-      console.log(err)
-    });
+    const fetchData = async () => {
+      await fetch("http://localhost:8000/")
+      .then((response) => {
+        return response.json()
+      })
+      .then((json) => {
+        setCounters(json)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+    }
+
+    fetchData()
   }, [])
+
+
+  useEffect(() => {
+
+    let updatedHistory = {}
+    Object.keys(counters).map((counter, index) => {
+
+      const fetchData = async () => {
+        await fetch(`http://localhost:8000/history/${counter}`)
+        .then((response) => {
+          return response.json()
+        })
+        .then((json) => {
+          updatedHistory[counter] = json
+          
+          // forceUpdate()
+
+          if (Object.keys(updatedHistory).length === Object.keys(counters).length) {
+            setHistory(updatedHistory)
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        }); 
+      }
+
+      fetchData()
+      return null
+    })
+  }, [counters])
+
 
   return (
     <Wrapper>
-      <h1>Stocks</h1>
+      <h1>Stonks</h1>
       <time>{today}</time>
       <input type="text" placeholder="Search" />
       <Listing>
@@ -142,10 +181,40 @@ const App = () => {
               <span className="company_name">{counters[counter].company_name}</span>
             </div>
             <div className="graph">
-
+              <AreaChart width={85} height={35} data={history[counter]}
+              margin={{ top: 2, right: 10, left: 10, bottom: 0 }}
+            >
+              <defs>
+                <linearGradient id="pos" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="rgba(118, 214, 113, 0.65)" />
+                  <stop offset="95%" stopColor="rgba(118, 214, 113, 0)" />
+                </linearGradient>
+                <linearGradient id="neg" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="rgba(234, 78, 61, 0.65)" />
+                  <stop offset="95%" stopColor="rgba(234, 78, 61, 0)" />
+                </linearGradient>
+              </defs>
+              {direction === 'pos' ?
+              <Area
+                type="monotone"
+                dataKey="closing_price"
+                stroke="#76d671"
+                strokeWidth="2"
+                fillOpacity=".65"
+                fill="url(#pos)"
+              /> :
+              <Area
+                type="monotone"
+                dataKey="closing_price"
+                stroke="#ea4e3d"
+                strokeWidth="2"
+                fillOpacity=".65"
+                fill="url(#neg)"
+              />}
+            </AreaChart>
             </div>
             <div className="price_change">
-              <span className="closing_price">{counters[counter].closing_price}</span>
+              <span className="closing_price">${counters[counter].closing_price}</span>
               <span className={`percent_change ${direction}`}>{counters[counter].percent_change}</span>
             </div>
           </li>)
